8f7cee8228b4b5b6069651d43ce1c57f2b0d6047
Jean Connelly <jconnelly@cfa.harvard.edu>
2024-04-21 15:45:47 -0400

    Update to work with obsid or date and maude data

--- kadi_apps/blueprints/pcad_acq/pcad_table.py
+++ kadi_apps/blueprints/pcad_acq/pcad_table.py
@@ -11,6 +11,7 @@ import numpy as np
 from cxotime import CxoTime
 from cheta import fetch
 from astropy.table import Table, Column
+import astropy.units as u
 from Quaternion import Quat
 import ska_quatutil
 import mica.starcheck
@@ -19,6 +20,7 @@ import agasc
 fetch.data_source.set('cxc', 'maude allow_subset=False')
 
 msids = [
+    "CVCDUCTR",
     "AOACASEQ",
     "AOACQSUC",
     "AOFREACQ",
@@ -60,11 +62,15 @@ slot_msids = [field + "%s" % slot for field in per_slot for slot in range(0, 8)]
 def deltas_vs_obc_quat(vals, times, catalog):
     # Ignore misalign
     aca_misalign = np.array([[1.0, 0, 0], [0, 1, 0], [0, 0, 1]])
+    q_ok = np.logical_and.reduce([~vals["AOATTQT1"].mask, ~vals["AOATTQT2"].mask,
+                                  ~vals["AOATTQT3"].mask, ~vals["AOATTQT4"].mask])
     q_att = Quat(
         q=np.array(
-            [vals["AOATTQT1"], vals["AOATTQT2"], vals["AOATTQT3"], vals["AOATTQT4"]]
+            [vals["AOATTQT1"][q_ok], vals["AOATTQT2"][q_ok],
+             vals["AOATTQT3"][q_ok], vals["AOATTQT4"][q_ok]]
         ).transpose()
     )
+
     Ts = q_att.transform
     acqs = catalog[(catalog["type"] == "BOT") | (catalog["type"] == "ACQ")]
 
@@ -80,39 +86,122 @@ def deltas_vs_obc_quat(vals, times, catalog):
         d_aca = np.dot(
             np.dot(aca_misalign, Ts.transpose(0, 2, 1)), star_pos_eci
         ).transpose()
+        dy[slot] = np.ma.masked_all(len(times))
+        dz[slot] = np.ma.masked_all(len(times))
         yag = np.arctan2(d_aca[:, 1], d_aca[:, 0]) * R2A
         zag = np.arctan2(d_aca[:, 2], d_aca[:, 0]) * R2A
-        dy[slot] = vals["AOACYAN{}".format(slot)] - yag
-        dz[slot] = vals["AOACZAN{}".format(slot)] - zag
+        dy[slot][q_ok] = vals["AOACYAN{}".format(slot)][q_ok] - yag
+        dz[slot][q_ok] = vals["AOACZAN{}".format(slot)][q_ok] - zag
 
     return dy, dz
 
 
-def get_acq_table(obsid):
+def get_time(obsid_or_date):
+    start_time = None
+    try:
+        obsid = int(obsid_or_date)
+    except ValueError:
+        obsid = get_obsid_for_date(obsid_or_date)
 
-    manvrs = events.manvrs.filter(obsid=obsid)
-    if not len(manvrs):
+    if obsid < 90000:
+        start_time = get_time_for_obsid(obsid)
+    else:
+        raise ValueError("Weird obsid: {}".format(obsid))
+
+    return start_time
+
+
+def get_obsid_for_date(date):
+    starcheck = mica.starcheck.get_starcheck_catalog_at_date(date)
+    return starcheck['obs']['obsid']
+
+
+def get_time_for_obsid_from_cmds(obsid):
+    import kadi.commands.states
+    states = kadi.commands.states.get_states(start=CxoTime.now() - 7 * u.day,
+    merge_identical=True, state_keys=['pcad_mode', 'obsid'])
+    ok = (states['pcad_mode'] == 'NPNT') & (states['obsid'] == obsid)
+    if not np.any(ok):
+        raise ValueError("No NPNT mode found for obsid {}".format(obsid))
+    start_time = states['tstart'][ok][0]
+    return start_time
+
+
+def get_time_for_obsid(obsid):
+    try:
+        manvrs = events.manvrs.filter(obsid=obsid)
+        if not len(manvrs):
+            raise ValueError("No manvr found for obsid {}".format(obsid))
+        manvr = manvrs[0]
+        start_time = CxoTime(manvr.acq_start).secs
+        return start_time
+    except:
+        start_time = get_time_for_obsid_from_cmds(obsid)
+        return start_time
+    else:
         return None
-    manvr = manvrs[0]
 
-    start_time = CxoTime(manvr.acq_start).secs
+
+def get_acq_table(obsid_or_date):
+
+    start_time = get_time(obsid_or_date)
+
+    if start_time is None:
+        raise ValueError("No start time found for {}".format(obsid_or_date))
+
+    if CxoTime(start_time).date < '2002:007':
+        raise ValueError("Tool not available for obsids before 2002:007")
+
     stop_time = start_time + (60 * 5)
     acq_data = fetch.MSIDset(msids + slot_msids, start_time, stop_time)
 
-    vals = Table([acq_data[col].vals for col in msids], names=msids)
-    for field in slot_msids:
-        vals.add_column(Column(name=field, data=acq_data[field].vals))
-        times = Table([acq_data["AOACASEQ"].times], names=["time"])
+    # Find a mod 4 = 0 time to start the data set
+    four_0 = acq_data['CVCDUCTR'].vals % 4 == 0
+    all_four_0 = acq_data['CVCDUCTR'].times[four_0]
+    if len(all_four_0) == 0:
+        raise ValueError("No mod 4 = 0 times found in the data set")
+
+    t0 = all_four_0[0]
+    max_time = acq_data['CVCDUCTR'].times[-1]
+    dtime = np.median(acq_data['CVCDUCTR'].times[1:] - acq_data['CVCDUCTR'].times[:-1])
+    times = np.arange(t0, max_time + dtime, dtime * 4)[:-1]
+
+    # Loop over the columns and make masked versions of the data
+    acq_data_masked = {}
+    for col in msids + slot_msids:
+        # initialize the column with masked values length of the grid_times
+        acq_data_masked[col] = np.ma.masked_all(
+            len(times),
+            dtype=acq_data[col].vals.dtype)
+        ok = (acq_data[col].times >= times[0]) & (acq_data[col].times <= times[-1])
+        for idx, time in enumerate(acq_data[col].times[ok]):
+            # find the index in the grid_times
+            gidx = np.searchsorted(times, time + (dtime * .9)) - 1
+            if gidx < len(times):
+                acq_data_masked[col][gidx] = acq_data[col].vals[idx]
+
+    vals = Table([acq_data_masked[col] for col in msids + slot_msids], names=msids + slot_msids)
+    vals['time'] = times
+    vals["AOREPEAT"].fill_value = '0'
+    vals["AOREPEAT"] = vals["AOREPEAT"].filled()
 
     def compress_data(data, dtime):
-        return data[data["AOREPEAT"] == "0 "], dtime[data["AOREPEAT"] == "0 "]
+        repeat = np.array([int(val) for val in data["AOREPEAT"]])
+        return data[repeat == 0], dtime[repeat == 0]
 
     vals, times = compress_data(vals, times)
+
+    # Find the time of the first AQXN row and filter the data to be after that
+    acq = np.where(vals["AOACASEQ"] == "AQXN")[0]
+    if len(acq) == 0:
+        raise ValueError("No AQXN found in the data set")
+    vals = vals[acq[0]:]
+    times = times[acq[0]:]
 
     # Get the catalog for the stars
     # This is used both to map ACQID to the right slot and
     # to get the star positions to estimate deltas later
-    starcheck = mica.starcheck.get_starcheck_catalog_at_date(manvr.start)
+    starcheck = mica.starcheck.get_starcheck_catalog_at_date(start_time)
     if "cat" not in starcheck:
         raise ValueError("No starcheck catalog found for {}".format(obsid))
     catalog = Table(starcheck["cat"])
@@ -125,7 +214,7 @@ def get_acq_table(obsid):
     index_for_slot = dict([(cat_row["slot"], cat_row["idx"]) for cat_row in catalog])
 
     # Estimate the offsets from the expected catalog positions
-    dy, dz = deltas_vs_obc_quat(vals, times["time"], catalog)
+    dy, dz = deltas_vs_obc_quat(vals, times, catalog)
     for slot in range(0, 8):
         vals.add_column(Column(name="dy{}".format(slot), data=dy[slot].data))
         vals.add_column(Column(name="dz{}".format(slot), data=dz[slot].data))
@@ -136,14 +225,15 @@ def get_acq_table(obsid):
     # make a list of dicts of the table
     simple_data = []
     kalm_start = None
-    for drow, trow in zip(vals, times):
+    for drow, time in zip(vals, times):
         if (kalm_start is None) and (drow["AOACASEQ"] == "KALM"):
-            kalm_start = trow["time"]
-        if (kalm_start is not None) and (trow["time"] > kalm_start + 5):
+            kalm_start = time
+        if (kalm_start is not None) and (time > kalm_start + 5):
             continue
         slot_data = {
             "slots": [],
-            "time": trow["time"],
+            "time": time,
+            "date": CxoTime(time).date,
             "aorfstr1_slot": slot_for_pos[int(drow["AORFSTR1"])],
             "aorfstr2_slot": slot_for_pos[int(drow["AORFSTR2"])],
         }
